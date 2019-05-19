4d00a6c03ef26691795419ab384d3b7490868430
Jesse Liang <jesseliang@ymail.com>
2019-05-19 00:59:25 -0700

    mongo dix?

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const assert = require('assert');
 const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
-const client = new MongoClient(mongoUrl);
-
 var app = express();
 
 var bodyParser = require('body-parser')
@@ -14,96 +12,109 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongo.connect(url, (err, client) => {
-	if (err) {
-		console.error(err);
-		return;
+var botAvailable = true;
+var currentPath = [];
+app.get('/', function (req, res) {
+	res.send('Hello World from GCE!');
+});
+app.get('/bot-free', function (req, res) {
+	res.json({ free: botAvailable });
+	res.status(200);
+	res.end();
+});
+app.get('/bot-instructions', function (req, res) {
+	res.json({path: currentPath});
+	res.end();
+});
+app.get('/bot-called', function (req, res) {
+	res.json({isCalled: !botAvailable});
+	res.end();
+});
+app.get('/bot-finished', function (req, res) {
+	botAvailable = true;
+});
+app.post('/bot-instructions', function (req, res) {
+	var name = "";
+	var path = [];
+	try {
+		name = req.body.name;
+		path = req.body.path;
+		res.sendStatus(200);
+		currentPath = path;
+		botAvailable = false;
+		console.log(req.body);
+	} catch (e) {
+		res.sendStatus(400);
 	}
-
-	const db = client.db('qpp');
-
-	const collection = db.collection('paths');
-
-	var botAvailable = true;
-	var currentPath = [];
-	app.get('/', function (req, res) {
-		res.send('Hello World from GCE!');
-	});
-	app.get('/bot-free', function (req, res) {
-		res.json({ free: botAvailable });
-		res.status(200);
-		res.end();
-	});
-	app.get('/bot-instructions', function (req, res) {
-		res.json({path: currentPath});
-		res.end();
-	});
-	app.get('/bot-called', function (req, res) {
-		res.json({isCalled: !botAvailable});
-		res.end();
-	});
-	app.get('/bot-finished', function (req, res) {
-		botAvailable = true;
-	});
-	app.post('/bot-instructions', function (req, res) {
-		var name = "";
-		var path = [];
-		try {
-			name = req.body.name;
-			path = req.body.path;
-			res.sendStatus(200);
-			currentPath = path;
-			botAvailable = false;
-			console.log(req.body);
-		} catch (e) {
-			res.sendStatus(400);
-		}
-	});
-	app.get('/get-all', function (req, res) {
-		console.log("get-all");
+});
+app.get('/get-all', function (req, res) {
+	console.log("get-all");
+	mongo.connect(url, (err, client) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			const db = client.db('qpp');
+			const collection = db.collection('paths');
 		collection.find({}).toArray((err, arr) => {
 			if (err) throw err;
 			res.json({paths: allPaths});
 			res.end();
 		});
+		client.close();
 	});
-	app.post('/edit-path', function (req, res) {
-		var name = "";
-		var path = [];
-		try {
-			name = req.body.name;
-			path = req.body.path;
-			res.sendStatus(200);
+});
+app.post('/edit-path', function (req, res) {
+	var name = "";
+	var path = [];
+	try {
+		name = req.body.name;
+		path = req.body.path;
+		res.sendStatus(200);
+		mongo.connect(url, (err, client) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			const db = client.db('qpp');
+			const collection = db.collection('paths');
 			var myquery = {name: name};
 			var myobj = {$set: {name: name, path: path}};
 			collection.updateOne(myquery, myobj, (err, item) => {
 				if (err) throw err;
 			});
-		} catch (e) {
-			res.sendStatus(400);
-		}
-		console.log(req.body);
-	});
-	app.post('/add-path', function (req, res) {
-		var name = "";
-		var path = [];
-		try {
-			name = req.body.name;
-			path = req.body.path;
-			res.sendStatus(200);
+			client.close();
+		});
+	} catch (e) {
+		res.sendStatus(400);
+	}
+	console.log(req.body);
+});
+app.post('/add-path', function (req, res) {
+	var name = "";
+	var path = [];
+	try {
+		name = req.body.name;
+		path = req.body.path;
+		res.sendStatus(200);
+		mongo.connect(url, (err, client) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			const db = client.db('qpp');
+			const collection = db.collection('paths');
 			var myobj = {name: name, path: path};
 			collection.insertOne(myobj, (err, result) => {
 				if (err) throw err;
 			});
-		} catch (e) {
-			res.sendStatus(400);
-		}
-		console.log(req.body);
-	});
-	app.listen(3000, function () {
-		console.log('Example app listening on port 3000!');
-	});
-
-	client.close();
-
+			client.close();
+		});
+	} catch (e) {
+		res.sendStatus(400);
+	}
+	console.log(req.body);
+});
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
 });
